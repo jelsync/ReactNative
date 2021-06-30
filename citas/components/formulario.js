f142118f0ce27605bd952d4b703b55bfefc62235
JelsynCeron <jelsync@gmail.com>
2021-06-29 22:59:31 -0600

    DateTimePickerModal para fecha y hora

--- citas/components/formulario.js
+++ citas/components/formulario.js
@@ -1,34 +1,92 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const Formulario = () => {
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+    const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+
+    const [fecha, setFecha] = useState('');
+    const [hora, setHora] = useState('');
+
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const confirmarFecha = (date) => {
+        const opciones = { year: 'numeric', month: 'long', day: '2-digit' }
+        setFecha(date.toLocaleDateString('es-ES', opciones));
+        hideDatePicker();
+    };
+
+    const showTimePicker = () => {
+        setTimePickerVisibility(true);
+    };
+
+    const hideTimePicker = () => {
+        setTimePickerVisibility(false);
+    };
+
+    const confirmarHora = (time) => {
+        const opciones = { hour: 'numeric', minute: '2-digit' }
+        setHora(time.toLocaleString('es-US', opciones));
+        hideTimePicker();
+    };
+
     return (
         <>
             <View style={styles.formulario}>
-                {/* <Text style={styles.titulo}>Administra tus Citas</Text> */}
                 <View>
                     <Text style={styles.label}>Paciente:</Text>
                     <TextInput
                         style={styles.input}
+                        onChangeText={(texto) => console.log(texto)}
+                    />
+                </View>
+                <View>
+                    <Text style={styles.label}>Telfono:</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={(texto) => console.log(texto)}
+                        keyboardType='number-pad'
                     />
                 </View>
                 <View>
                     <Text style={styles.label}>Sintomas:</Text>
                     <TextInput
+                        multiline
                         style={styles.input}
-                    />
-                </View>
-                <View>
-                    <Text style={styles.label}>Hora:</Text>
-                    <TextInput
-                        style={styles.input}
+                        onChangeText={(texto) => console.log(texto)}
+                        keyboardType='number-pad'
                     />
                 </View>
                 <View>
                     <Text style={styles.label}>Fecha:</Text>
-                    <TextInput
-                        style={styles.input}
+                    <Button title="Elije una fecha" onPress={showDatePicker} />
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={confirmarFecha}
+                        onCancel={hideDatePicker}
+                        locale='es_ES'
                     />
+                    <Text>{fecha}</Text>
+                </View>
+                <View>
+                    <Text style={styles.label}>Hora:</Text>
+                    <Button title="Elije una hora" onPress={showTimePicker} />
+                    <DateTimePickerModal
+                        isVisible={isTimePickerVisible}
+                        mode="time"
+                        onConfirm={confirmarHora}
+                        onCancel={hideTimePicker}
+                        locale='es_ES'
+                    />
+                    <Text>{hora}</Text>
                 </View>
             </View>
         </>
