f361db15397250dc8ddb648b9d3c0c76e849a10c
JelsynCeron <jelsync@gmail.com>
2021-07-01 10:34:15 -0600

    Imagen como banner uso de flexDirection y flex

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -1,46 +1,28 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { View, StyleSheet, Image, Text, ScrollView } from 'react-native';
 
 const App = () => {
   return (
     <>
       <View>
-        <View style={styles.caja1}></View>
-        <View style={styles.caja2}></View>
-        <View style={styles.caja3}></View>
-        <View style={styles.caja4}></View>
+        <View style={styles.contenedor}>
+          <Image
+            style={styles.banner}
+            source={require('./assets/img/bg.jpg')}
+          />
+        </View>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-
   contenedor: {
-    backgroundColor: 'cornflowerblue',
+    flexDirection: 'row'
+  },
+  banner: {
+    height: 250,
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  caja1: {
-    padding: 20,
-    backgroundColor: 'navy'
-  },
-  caja2: {
-    padding: 20,
-    backgroundColor: 'yellow'
-  },
-  caja3: {
-    padding: 20,
-    backgroundColor: 'green'
-  },
-  caja4: {
-    padding: 20,
-    backgroundColor: 'red'
   },
 });
 
