b256967cc27ca632dd3becf9122cf6bb035b2ecc
JelsynCeron <jelsync@gmail.com>
2021-07-03 13:57:18 -0600

    Animated

---
+++ animated/components/Animacion1.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, Animated } from 'react-native';
+
+const Animacion1 = () => {
+
+    const [animacion] = useState(new Animated.Value(0));
+
+    useEffect(() => {
+        Animated.timing(
+            animacion, {
+            toValue: 1,
+            duration: 1500,
+            useNativeDriver: false,
+        }).start();
+    }, []);
+
+
+    return (
+        <Animated.View
+            style={{
+                opacity: animacion
+            }}
+        >
+            <Text style={styles.texto}>Animacion 1</Text>
+        </Animated.View >
+    );
+}
+
+const styles = StyleSheet.create({
+    texto: {
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
+
+export default Animacion1;
